06f4829b1993a6d6179cd95ab97d3f5cec6d0bf7
caspero_iv <nwaforuchechukwu2007@gmail.com>
2020-05-26 16:36:14 +0100

    Updates

---
+++ precache-manifest.63defa4ad9990284a29617c6b7666cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d85ae7e4760e25db926a9d9e9a8a37a",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "1bd32022d5b21e766199",
+    "url": "/to-do-list/static/css/main.e2aadd9c.chunk.css"
+  },
+  {
+    "revision": "c43cd9e3164ef3b47ab7",
+    "url": "/to-do-list/static/js/2.b527764d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.b527764d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bd32022d5b21e766199",
+    "url": "/to-do-list/static/js/main.e07145fd.chunk.js"
+  },
+  {
+    "revision": "4dd89752f846897d86f5",
+    "url": "/to-do-list/static/js/runtime-main.06d3fbd6.js"
+  },
+  {
+    "revision": "305ef23df2b9887eb9a20c90f41df4da",
+    "url": "/to-do-list/static/media/62.305ef23d.png"
+  }
+]);
